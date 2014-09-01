8e24ade60fd1ad927c215e0d0a49911496f18095
Ben Cherrington <Cocanuta@gmail.com>
2014-09-01 19:16:42 +0100

    First

---
+++ Scripts/Inventory.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Inventory : MonoBehaviour {
+	
+	public int itemID;
+	public string itemName;
+	public string itemDescription;
+	
+	public Items itemScript;
+
+	public int[] inv;
+
+
+	// Use this for initialization
+	void Start () {
+
+		inv = new int[2];
+
+		inv [0] = 1;
+		inv [1] = 8;
+
+		for (int i = 0; i < inv.Length; i++) {
+			
+			itemScript.Info(i, out itemName, out itemDescription);
+			
+			Debug.Log(inv[i] + " x " + itemName + " added to inventory.");
+			
+			
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+
+
+	}
+
+}
---
+++ Scripts/Items.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Items : MonoBehaviour {
+
+
+	public void Info(int i, out string n, out string d) {
+
+		n = "null item";
+		d = "null description.";
+
+		switch(i) {
+
+		case 0:
+			n = "test item 0";
+			d = "test description 0.";
+			break;
+		case 1:
+			n = "test item 1";
+			d = "test description 1.";
+			break;
+		}
+
+	}
+}
